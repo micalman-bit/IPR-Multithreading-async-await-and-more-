4f2c4f7250eb322ff8e2e4dbf5e7601c32dd8191
Andrey Samchenko <andreysamchenko@MacBook-Pro-Andrey.local>
2024-12-17 13:35:07 +0800

    Start

---
+++ Start/GettingStartedAsyncAwait/GettingStartedAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GettingStartedAsyncAwaitApp.swift
+//  GettingStartedAsyncAwait
+//
+//  Created by Andrey Samchenko
+//
+
+import SwiftUI
+
+@main
+struct GettingStartedAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
